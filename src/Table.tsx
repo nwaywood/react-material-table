5694e4a681c0310e03347fbe0f8ba9aaf5c50695
Antony Targett <atargett@au1.ibm.com>
2019-01-16 13:52:59 +1100

    fix: Added back table.tsx file that was removed during merge and rename to Table.tsx. Removed shadow-variables. Made file conform with tslint rules. Added a tslint ignore for the Array instantiation

--- src/Table.tsx
+++ src/Table.tsx
@@ -21,10 +21,10 @@ interface Column {
         dataName: string,
     };
     cellValue?: (
-                rowData: object,
-                toggleAccordion: () => any,
-                isOpen: boolean,
-        ) => string | JSX.Element;
+        rowData: object,
+        toggleAccordion: () => any,
+        isOpen: boolean,
+    ) => string | JSX.Element;
     colWidthProportion?: number;
     sort?: boolean | ((a: object, b: object) => number);
 }
@@ -34,12 +34,139 @@ interface Sort {
     dataName: string;
 }
 
-const Table = (props: Props) => {
-    const [test, setTest] = React.useState(3);
-    return <Div>{test}</Div>;
+const ReactMaterialTable = (props: Props) => {
+    // initialise array with all false values because
+    // no items are open initially
+    // tslint:disable-next-line
+    const bools = new Array(props.data.length).fill(false);
+    const [openIndexes, setOpenIndexes] = React.useState(bools);
+
+    const onRowClick = (index: number) => {
+        return () => {
+            // update the openIndexes bools
+            const boolArray = [...openIndexes];
+            boolArray[index] = !boolArray[index];
+            setOpenIndexes(boolArray);
+        };
+    };
+
+    const renderRow = (columnArray: Column[]) => {
+        return (rowItem: object, index: number) => (
+            <div key={index}>
+                <TableRowDiv className="table-row" onClick={onRowClick(index)}>
+                    {columnArray.map(
+                        renderRowColumn(
+                            rowItem,
+                            calcTotalProportions(props.columns),
+                            index,
+                        ),
+                    )}
+                </TableRowDiv>
+                {/* {openIndexes[index]
+                    ? props.getReactElement(props, rowItem, index)
+                    : null} */}
+            </div>
+        );
+    };
+
+    const renderRowColumn = (
+        rowItem: object,
+        totalWidthProportions: number,
+        index: number,
+    ) => (columnItem: Column, columnIndex: number) => {
+        let tmp;
+        if (columnItem.cellValue) {
+            tmp = columnItem.cellValue(rowItem, () => null, openIndexes[index]);
+        } else {
+            const cellData = rowItem[columnItem.columnHeader.dataName];
+            if (cellData === undefined) {
+                console.warn(
+                    `dataName "${
+                        columnItem.columnHeader.dataName
+                    }" does not match any of the data fields. Make sure the key/value pair matches the dataname`,
+                );
+            }
+            tmp = cellData;
+        }
+        return (
+            <TableRowItemDiv
+                key={columnIndex}
+                totalWidthProportions={totalWidthProportions}
+                colWidthProportion={columnItem.colWidthProportion}
+            >
+                {tmp}
+            </TableRowItemDiv>
+        );
+    };
+
+    const { className, data, columns } = props;
+    return (
+        <TableDiv className={className}>
+            <TableHeaderRowDiv className="table-header-row">
+                {columns.map(renderHeaderColumn(calcTotalProportions(columns)))}
+            </TableHeaderRowDiv>
+            {data.map(renderRow(columns))}
+        </TableDiv>
+    );
 };
 
-const Div = styled.div`
-    color: red;
+// sum up all the supplied proportions or default to 1
+const calcTotalProportions = (columns: Column[]): number =>
+    columns.reduce(
+        (acc, val) =>
+            acc + (val.colWidthProportion ? val.colWidthProportion : 1),
+        0,
+    );
+
+const renderHeaderColumn = totalWidthProportions => (
+    item: Column,
+    index: number,
+) => (
+    <TableHeaderItemDiv
+        key={index}
+        totalWidthProportions={totalWidthProportions}
+        colWidthProportion={item.colWidthProportion}
+        className="table-header-column"
+    >
+        {item.columnHeader.displayName}
+    </TableHeaderItemDiv>
+);
+
+const TableDiv = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
 `;
-export default Table;
+const TableHeaderRowDiv = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    height: 56px;
+    align-items: center;
+`;
+const TableHeaderItemDiv = styled.div<{
+    colWidthProportion: number
+    totalWidthProportions: number,
+}>`
+    width: ${props =>
+        `${((props.colWidthProportion || 1) / props.totalWidthProportions) *
+            100}%`};
+    padding: 0px 10px;
+`;
+const TableRowDiv = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    cursor: pointer;
+
+    height: 48px;
+    align-items: center;
+`;
+const TableRowItemDiv = styled.div<{
+    colWidthProportion: number
+    totalWidthProportions: number,
+}>`
+    width: ${props =>
+        `${((props.colWidthProportion || 1) / props.totalWidthProportions) *
+            100}%`};
+    padding: 0px 10px;
+`;
+
+export default ReactMaterialTable;
