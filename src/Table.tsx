9aff704fea02de5f4b423a27fbd2f02ae90400c8
Antony Targett <atargett@au1.ibm.com>
2019-01-18 09:16:18 +1100

    fix: Fixed tslint-prettier integration. Both plugins should now be in sync

--- src/Table.tsx
+++ src/Table.tsx
@@ -4,123 +4,127 @@ import DownwardIconSvg from "./assets/icons/DownwardIcon.svg"
 import UpwardIconSvg from "./assets/icons/UpwardIcon.svg"
 
 type Props = {
-    data: object[];
-    columns: Column[];
-    accordion?: (rowData: object) => JSX.Element;
-    onRowSelection?: (o: { rowData: object; toggleAccordion: () => any }) => any;
-    className?: string;
-    header?: string;
-    loading?: boolean;
-    defaultSort?: Sort;
-    sortCallback?: (sort: Sort) => any;
-    defaultHiddenColumns?: string[];
-    headerCustomContent?: JSX.Element;
+    data: object[]
+    columns: Column[]
+    accordion?: (rowData: object) => JSX.Element
+    onRowSelection?: (o: { rowData: object; toggleAccordion: () => any }) => any
+    className?: string
+    header?: string
+    loading?: boolean
+    defaultSort?: Sort
+    sortCallback?: (sort: Sort) => any
+    defaultHiddenColumns?: string[]
+    headerCustomContent?: JSX.Element
 }
 
 type Column = {
     columnHeader: {
-        displayName: string;
-        dataName: string;
-    };
+        displayName: string
+        dataName: string
+    }
     cellValue?: (
-    rowData: object,
-    toggleAccordion: () => any,
-    isOpen: boolean
-  ) => string | JSX.Element;
-    colWidthProportion?: number;
-    sort?: boolean | ((a: object, b: object) => number);
+        rowData: object,
+        toggleAccordion: () => any,
+        isOpen: boolean
+    ) => string | JSX.Element
+    colWidthProportion?: number
+    sort?: boolean | ((a: object, b: object) => number)
 }
 
 type Sort = {
-    order?: string;
-    dataName: string;
+    order?: string
+    dataName: string
 }
 
 const ReactMaterialTable = (props: Props) => {
-  // initialise array with all false values because
-  // no items are open initially
-  // tslint:disable-next-line
+    // initialise array with all false values because
+    // no items are open initially
+    // tslint:disable-next-line
   const bools = new Array(props.data.length).fill(false);
     const [openIndexes, setOpenIndexes] = React.useState(bools)
 
     const toggleAccordion = (index: number) => () => {
-    // update the openIndexes bools
+        // update the openIndexes bools
         const tmp = [...openIndexes]
         tmp[index] = !tmp[index]
         setOpenIndexes(tmp)
     }
 
     const createRenderRowClickCallback = (
-    onRowSelection,
-    rowItem: object,
-    toggleAccordionFn: () => void
-  ) => () => {
-      return onRowSelection({ rowItem, toggleAccordionFn })
-  }
+        onRowSelection,
+        rowItem: object,
+        toggleAccordionFn: () => void
+    ) => () => {
+        return onRowSelection({ rowItem, toggleAccordionFn })
+    }
     const renderRow = (columnArray: Column[]) => {
         return (rowItem: object, index: number) => (
-      <div key={index}>
-        <TableRowDiv
-          className="table-row"
-          // only add onClick listener if user has supplied a function
-          onClick={
-            props.onRowSelection
-              ? createRenderRowClickCallback(
-                  props.onRowSelection,
-                  rowItem,
-                  toggleAccordion(index)
-                )
-              : () => false
-          }
-        >
-          {columnArray.map(
-            renderRowColumn(rowItem, calcTotalProportions(props.columns), index)
-          )}
-        </TableRowDiv>
-        {openIndexes[index] && props.accordion
-          ? props.accordion(rowItem)
-          : null}
-      </div>
-    )
+            <div key={index}>
+                <TableRowDiv
+                    className="table-row"
+                    // only add onClick listener if user has supplied a function
+                    onClick={
+                        props.onRowSelection
+                            ? createRenderRowClickCallback(
+                                  props.onRowSelection,
+                                  rowItem,
+                                  toggleAccordion(index)
+                              )
+                            : () => false
+                    }
+                >
+                    {columnArray.map(
+                        renderRowColumn(
+                            rowItem,
+                            calcTotalProportions(props.columns),
+                            index
+                        )
+                    )}
+                </TableRowDiv>
+                {openIndexes[index] && props.accordion
+                    ? props.accordion(rowItem)
+                    : null}
+            </div>
+        )
     }
 
     const renderRowColumn = (
-    rowItem: object,
-    totalWidthProportions: number,
-    index: number
-  ) => (columnItem: Column, columnIndex: number) => {
-      let tmp
-      if (columnItem.cellValue) {
-          tmp = columnItem.cellValue(rowItem, () => null, openIndexes[index])
-      } else {
-          const cellData = rowItem[columnItem.columnHeader.dataName]
-          if (cellData === undefined) {
-              console.warn(
-          `dataName "${
-            columnItem.columnHeader.dataName
-          }" does not match any of the data fields. Make sure the key/value pair matches the dataname`
+        rowItem: object,
+        totalWidthProportions: number,
+        index: number
+    ) => (columnItem: Column, columnIndex: number) => {
+        let tmp
+        if (columnItem.cellValue) {
+            tmp = columnItem.cellValue(rowItem, () => null, openIndexes[index])
+        } else {
+            const cellData = rowItem[columnItem.columnHeader.dataName]
+            if (cellData === undefined) {
+                console.warn(
+                    `dataName "${
+                        columnItem.columnHeader.dataName
+                    }" does not match any of the data fields. Make sure the key/value pair matches the dataname`
+                )
+            }
+            tmp = cellData
+        }
+        return (
+            <TableRowItemDiv
+                key={columnIndex}
+                totalWidthProportions={totalWidthProportions}
+                colWidthProportion={columnItem.colWidthProportion}
+            >
+                {tmp}
+            </TableRowItemDiv>
         )
-          }
-          tmp = cellData
-      }
-      return (
-      <TableRowItemDiv
-        key={columnIndex}
-        totalWidthProportions={totalWidthProportions}
-        colWidthProportion={columnItem.colWidthProportion}
-      >
-        {tmp}
-      </TableRowItemDiv>
-      )
-  }
+    }
 
     const { className, data, columns, defaultSort } = props
     const [sortedColumn, setSortedColumn] = React.useState(defaultSort)
     const sortData = (
-    rows: object[],
-    allColumns: Column[],
-    currentlySortedColumn?: Sort
-  ) => {
+        rows: object[],
+        allColumns: Column[],
+        currentlySortedColumn?: Sort
+    ) => {
         if (currentlySortedColumn) {
             console.log("I should sort with this", currentlySortedColumn)
             return rows
@@ -129,27 +133,28 @@ const ReactMaterialTable = (props: Props) => {
     }
 
     return (
-    <TableDiv className={className}>
-      <TableHeaderRowDiv className="table-header-row">
-        {columns.map(
-          renderHeaderColumn(
-            calcTotalProportions(columns),
-            setSortedColumn,
-            sortedColumn
-          )
-        )}
-      </TableHeaderRowDiv>
-      {sortData(data, columns, sortedColumn).map(renderRow(columns))}
-    </TableDiv>
+        <TableDiv className={className}>
+            <TableHeaderRowDiv className="table-header-row">
+                {columns.map(
+                    renderHeaderColumn(
+                        calcTotalProportions(columns),
+                        setSortedColumn,
+                        sortedColumn
+                    )
+                )}
+            </TableHeaderRowDiv>
+            {sortData(data, columns, sortedColumn).map(renderRow(columns))}
+        </TableDiv>
     )
 }
 
 // sum up all the supplied proportions or default to 1
 const calcTotalProportions = (columns: Column[]): number =>
-  columns.reduce(
-    (acc, val) => acc + (val.colWidthProportion ? val.colWidthProportion : 1),
-    0
-  )
+    columns.reduce(
+        (acc, val) =>
+            acc + (val.colWidthProportion ? val.colWidthProportion : 1),
+        0
+    )
 const renderArrow = (sortable: boolean, isCurrentColumn: boolean, order?) => {
     if (isCurrentColumn && (!order || order === "asc")) {
         return <img src={UpwardIconSvg} />
@@ -163,14 +168,17 @@ const renderArrow = (sortable: boolean, isCurrentColumn: boolean, order?) => {
     return null
 }
 const setCurrentSortColumn = (
-  isCurrentColumn: boolean,
-  column: Column,
-  setSortedColumn: (s: Sort) => void,
-  sortedColumn?: Sort
+    isCurrentColumn: boolean,
+    column: Column,
+    setSortedColumn: (s: Sort) => void,
+    sortedColumn?: Sort
 ) => {
     const sortObj = { dataName: column.columnHeader.dataName, order: "asc" }
     if (column.sort && isCurrentColumn) {
-        if (sortedColumn && (sortedColumn.order === "asc" || !sortedColumn.order)) {
+        if (
+            sortedColumn &&
+            (sortedColumn.order === "asc" || !sortedColumn.order)
+        ) {
             sortObj.order = "desc"
         } else if (sortedColumn && sortedColumn.order === "desc") {
             sortObj.order = "asc"
@@ -180,88 +188,88 @@ const setCurrentSortColumn = (
 }
 
 const renderHeaderColumn = (
-  totalWidthProportions: number,
-  setSortedColumn: (s: Sort) => void,
-  sortedColumn?: Sort
+    totalWidthProportions: number,
+    setSortedColumn: (s: Sort) => void,
+    sortedColumn?: Sort
 ) => (item: Column, index: number) => {
     const isCurrentColumn = sortedColumn
-    ? item.columnHeader.dataName === sortedColumn.dataName
-    : false
+        ? item.columnHeader.dataName === sortedColumn.dataName
+        : false
     return (
-    <TableHeaderItemDiv
-      key={index}
-      totalWidthProportions={totalWidthProportions}
-      colWidthProportion={item.colWidthProportion}
-      className="table-header-column"
-      onClick={
-        item.sort
-          ? () =>
-              setCurrentSortColumn(
+        <TableHeaderItemDiv
+            key={index}
+            totalWidthProportions={totalWidthProportions}
+            colWidthProportion={item.colWidthProportion}
+            className="table-header-column"
+            onClick={
+                item.sort
+                    ? () =>
+                          setCurrentSortColumn(
+                              isCurrentColumn,
+                              item,
+                              setSortedColumn,
+                              sortedColumn
+                          )
+                    : () => false
+            }
+            sortable={!!item.sort}
+        >
+            {item.columnHeader.displayName}
+            {renderArrow(
+                !!item.sort,
                 isCurrentColumn,
-                item,
-                setSortedColumn,
-                sortedColumn
-              )
-          : () => false
-      }
-      sortable={!!item.sort}
-    >
-      {item.columnHeader.displayName}
-      {renderArrow(
-        !!item.sort,
-        isCurrentColumn,
-        sortedColumn && sortedColumn.order
-      )}
-    </TableHeaderItemDiv>
+                sortedColumn && sortedColumn.order
+            )}
+        </TableHeaderItemDiv>
     )
 }
 
 const HoverableArrow = styled.img`
-  display: none;
+    display: none;
 `
 
 const TableDiv = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
+    display: flex;
+    flex-flow: column nowrap;
 `
 const TableHeaderRowDiv = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  height: 56px;
-  align-items: center;
-  border-bottom: 1px solid #827c7c57;
+    display: flex;
+    flex-flow: row nowrap;
+    height: 56px;
+    align-items: center;
+    border-bottom: 1px solid #827c7c57;
 `
 const TableHeaderItemDiv = styled.div<{
-    colWidthProportion?: number;
-    totalWidthProportions: number;
-    onClick: any;
-    sortable: boolean;
+    colWidthProportion?: number
+    totalWidthProportions: number
+    onClick: any
+    sortable: boolean
 }>`
-  width: ${props =>
-    `${((props.colWidthProportion || 1) / props.totalWidthProportions) *
-      100}%`};
-  &:hover > #hoverableArrow {
-    display: inline;
-  }
-  padding: 0px 10px;
-  cursor: ${props => (props.sortable ? "pointer" : "cursor")};
+    width: ${props =>
+        `${((props.colWidthProportion || 1) / props.totalWidthProportions) *
+            100}%`};
+    &:hover > #hoverableArrow {
+        display: inline;
+    }
+    padding: 0px 10px;
+    cursor: ${props => (props.sortable ? "pointer" : "cursor")};
 `
 const TableRowDiv = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  cursor: pointer;
-  border-bottom: 1px solid #827c7c57;
-  height: 48px;
-  align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    cursor: pointer;
+    border-bottom: 1px solid #827c7c57;
+    height: 48px;
+    align-items: center;
 `
 const TableRowItemDiv = styled.div<{
-    colWidthProportion?: number;
-    totalWidthProportions: number;
+    colWidthProportion?: number
+    totalWidthProportions: number
 }>`
-  width: ${props =>
-    `${((props.colWidthProportion || 1) / props.totalWidthProportions) *
-      100}%`};
-  padding: 0px 10px;
+    width: ${props =>
+        `${((props.colWidthProportion || 1) / props.totalWidthProportions) *
+            100}%`};
+    padding: 0px 10px;
 `
 
 export default ReactMaterialTable
