997dac3989e59d5090a5135905a1ce8b139b8a89
Nick Waywood <nwaywood@au1.ibm.com>
2019-02-01 11:28:40 +1100

    Fixed import path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict"
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/react-material-table.cjs.production.js');
+if (process.env.NODE_ENV === "production") {
+    module.exports = require("./react-material-table.cjs.production.js")
 } else {
-  module.exports = require('./dist/react-material-table.cjs.development.js');
+    module.exports = require("./react-material-table.cjs.development.js")
 }
